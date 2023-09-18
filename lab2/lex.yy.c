02e7e1532c3159b6a962b1d0d149268bc5a01a42
admin <ubuntu@ubuntu22.04.ubuntu>
2023-09-18 13:48:54 +0300

    fix

--- lab2/lex.yy.c
+++ lab2/lex.yy.c
@@ -360,10 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[21] =
+static const flex_int16_t yy_accept[14] =
     {   0,
-        0,    0,    0,    0,    0,    0,    0,    0,    7,    5,
-        5,    4,    6,    2,    0,    0,    4,    1,    3,    0
+        0,    0,    7,    5,    5,    4,    2,    0,    0,    4,
+        1,    3,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -403,36 +403,32 @@ static const YY_CHAR yy_meta[10] =
         1,    1,    2,    2,    3,    4,    5,    6,    7
     } ;
 
-static const flex_int16_t yy_base[27] =
+static const flex_int16_t yy_base[19] =
     {   0,
-        0,    0,    0,    0,    0,    0,    0,    0,   22,   36,
-        2,    0,   36,    0,    0,    0,    0,    0,    0,   36,
-       11,   17,   23,   26,   28,   19
+        0,    0,   15,   29,    2,    0,    0,    0,    0,    0,
+        0,    0,   29,   10,   16,   19,   21,   12
     } ;
 
-static const flex_int16_t yy_def[27] =
+static const flex_int16_t yy_def[19] =
     {   0,
-       20,    1,   21,   21,   21,   21,   21,   21,   20,   20,
-       22,   23,   20,   24,   25,   26,   23,   25,   26,    0,
-       20,   20,   20,   20,   20,   20
+       13,    1,   13,   13,   14,   15,   16,   17,   18,   15,
+       17,   18,    0,   13,   13,   13,   13,   13
     } ;
 
-static const flex_int16_t yy_nxt[46] =
+static const flex_int16_t yy_nxt[39] =
     {   0,
-       10,   10,   11,   12,   12,   12,   10,   10,   10,   15,
-       16,   13,   13,   13,   13,   13,   13,   13,   14,   14,
-       19,   20,   14,   14,   17,   17,   17,   14,   14,   18,
-       18,   18,   18,   20,   18,    9,   20,   20,   20,   20,
-       20,   20,   20,   20,   20
+        4,    4,    5,    6,    6,    6,    4,    4,    4,    8,
+        9,    7,    7,   12,   13,    7,    7,   10,   10,   10,
+        7,    7,   11,   11,   11,   11,   13,   11,    3,   13,
+       13,   13,   13,   13,   13,   13,   13,   13
     } ;
 
-static const flex_int16_t yy_chk[46] =
+static const flex_int16_t yy_chk[39] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,   11,
-       11,   21,   21,   21,   21,   21,   21,   21,   22,   22,
-       26,    9,   22,   22,   23,   23,   23,   24,   24,   25,
-       25,   25,   25,    0,   25,   20,   20,   20,   20,   20,
-       20,   20,   20,   20,   20
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    5,
+        5,   14,   14,   18,    3,   14,   14,   15,   15,   15,
+       16,   16,   17,   17,   17,   17,    0,   17,   13,   13,
+       13,   13,   13,   13,   13,   13,   13,   13
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -453,15 +449,10 @@ char *yytext;
 #line 2 "convert.l"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#line 458 "lex.yy.c"
-
-#line 460 "lex.yy.c"
+#line 453 "lex.yy.c"
+#line 454 "lex.yy.c"
 
 #define INITIAL 0
-#define HEX 1
-#define OCT 2
-#define BIN 3
 
 #ifndef YY_NO_UNISTD_H
 /* Special case for "unistd.h", since it is non-ANSI. We include it way
@@ -676,9 +667,9 @@ YY_DECL
 		}
 
 	{
-#line 9 "convert.l"
+#line 6 "convert.l"
 
-#line 682 "lex.yy.c"
+#line 673 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -705,13 +696,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 21 )
+				if ( yy_current_state >= 14 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 36 );
+		while ( yy_base[yy_current_state] != 29 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -737,57 +728,49 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 10 "convert.l"
+#line 7 "convert.l"
 {
-    int decimal = (int)strtol(yytext, NULL, 16);
-    printf("%d", decimal);
+    printf("%d", (int)strtol(yytext, NULL, 16));
 }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 15 "convert.l"
+#line 11 "convert.l"
 {
-    int decimal = (int)strtol(yytext, NULL, 8);
-    printf("%d", decimal);
+    printf("%d", (int)strtol(yytext, NULL, 8));
 }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 20 "convert.l"
+#line 15 "convert.l"
 {
-    int decimal = 0;
-    for (int i = 2; yytext[i] != '\0'; i++) {
-        decimal = decimal * 2 + (yytext[i] - '0');
-    }
+    char* binaryValue = yytext + 2;  // Пропускаем "0b"
+    int decimal = strtol(binaryValue, NULL, 2);
     printf("%d", decimal);
 }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 28 "convert.l"
+#line 21 "convert.l"
 {
-    int decimal = atoi(yytext);
-    printf("%d", decimal);
+    printf("%d", atoi(yytext));
 }
 	YY_BREAK
 case 5:
 /* rule 5 can match eol */
 YY_RULE_SETUP
-#line 33 "convert.l"
+#line 25 "convert.l"
 {
     printf("%s", yytext);
 }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 37 "convert.l"
+#line 28 "convert.l"
 ECHO;
 	YY_BREAK
-#line 787 "lex.yy.c"
+#line 773 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
-case YY_STATE_EOF(HEX):
-case YY_STATE_EOF(OCT):
-case YY_STATE_EOF(BIN):
 	yyterminate();
 
 	case YY_END_OF_BUFFER:
@@ -1083,7 +1066,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 21 )
+			if ( yy_current_state >= 14 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1111,11 +1094,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 21 )
+		if ( yy_current_state >= 14 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 20);
+	yy_is_jam = (yy_current_state == 13);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1791,8 +1774,12 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 37 "convert.l"
+#line 28 "convert.l"
 
+
+int yywrap() {
+    return 1;  // Возвращаем 1, чтобы сообщить, что анализ завершен
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
